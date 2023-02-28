e11e8210f9595d55e74598d1296bbee3ae4b2f44
Keroles-Nady <Keroles_N@outlook.com>
2023-03-01 01:56:31 +0200

    Task #10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,8 +10,8 @@ int _atoi(char *s)
 {
 	int i = 1;
 	unsigned int num = 0;
-	
-	do {
+
+	do{
 		if (*s == '-')
 			i *= -1;
 		else if (*s >= '0' && *s <= '9')
@@ -19,6 +19,6 @@ int _atoi(char *s)
 		else if (num > 0)
 			break;
 	} while (*s++);
-	
-	return (num * i);
+
+	return(num * i);
 }
